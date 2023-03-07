b3a30a4b216d6572051cb8fe56a6d02e8d580f94
shiina <jaren1418@gmail.com>
2023-03-07 10:15:23 +0800

    cards.cdb 更新逻辑

--- make_db.py
+++ make_db.py
@@ -34,15 +34,12 @@ if should_update or not CARDS_JSON_PATH.exists():
     if response.status_code == 200:
         with zipfile.ZipFile(io.BytesIO(response.content)) as z:
             with z.open('cards.json') as f:
-                text = f.read()
-                dict_raw = json.loads(text)
-                CARDS_JSON_PATH.write_text(text.decode('utf8'), encoding='utf8')
-            # z.extractall(RAW_DB_DIR)
+                CARDS_JSON_PATH.write_text(f.read().decode('utf8'), encoding='utf8')
         VERSION_PATH.write_text(version)
     else:
         raise Exception(response.text)
-else:
-    dict_raw = json.loads(CARDS_JSON_PATH.read_text(encoding='utf8'))
+
+dict_raw = json.loads(CARDS_JSON_PATH.read_text(encoding='utf8'))
 
 dict_small = {}
 for cid, d in dict_raw.items():
@@ -55,31 +52,36 @@ utils.ID2DATA_PATH.write_text(
 )
 
 # download manually; no need to update regularly
-# https://github.com/mycard/ygopro-database/raw/master/locales/zh-CN/cards.cdb
+SOURCE_CARDS_CDB_URL = 'https://github.com/mycard/ygopro-database/raw/master/locales/zh-CN/cards.cdb'
 CARDS_CDB_PATH = RAW_DB_DIR / 'cards.cdb'
 
-if CARDS_CDB_PATH.exists():
-    import pandas as pd
-    # normalize card id for cards with alternate artworks
-    with sqlite3.connect(CARDS_CDB_PATH) as connection:
-        df_data = pd.read_sql(
-            'SELECT texts.name, datas.id, datas.alias '
-            'FROM datas JOIN texts ON datas.id = texts.id '
-            'WHERE datas.alias != 0',
-            connection,
-        )
+if not CARDS_CDB_PATH.exists():
+    response = requests.get(SOURCE_CARDS_CDB_URL)
+    if response.status_code == 200:
+        with open(CARDS_CDB_PATH, 'wb') as f:
+            f.write(response.content)
+    else:
+        raise Exception(response.text)
 
-    alias2id_small = {}
-    for _, row in df_data.iterrows():
-        alias = row['alias']
-        id_ = row['id']
-        if id_ in dict_small and alias not in dict_small:
-            alias2id_small[alias] = id_
-        if alias in dict_small and id_ not in dict_small:
-            alias2id_small[id_] = alias
+# normalize card id for cards with alternate artworks
+with sqlite3.connect(CARDS_CDB_PATH) as connection:
+    cur = connection.cursor()
+    data = cur.execute(
+        'SELECT texts.name, datas.id, datas.alias '
+        'FROM datas JOIN texts ON datas.id = texts.id '
+        'WHERE datas.alias != 0'
+    ).fetchall()
 
-    utils.ALIAS2ID_PATH.write_text(json.dumps(alias2id_small, indent=2))
+alias2id_small = {}
+for row in data:
+    alias = row[2]
+    id_ = row[1]
+    if id_ in dict_small and alias not in dict_small:
+        alias2id_small[alias] = id_
+    if alias in dict_small and id_ not in dict_small:
+        alias2id_small[id_] = alias
 
+utils.ALIAS2ID_PATH.write_text(json.dumps(alias2id_small, indent=2))
 
 OLD2ID_URL = 'https://ygocdb.com/api/v0/idChangelog.jsonp'
 response = requests.get(OLD2ID_URL)
